dac771a4bcc01534dcd637f19d9e8c8780559b19
miguelprofissional <miguelprofissional8@gmail.com>
2022-04-12 18:34:02 -0300

    Aula 04

--- Aula 03/aula03.cs
+++ Aula 03/aula03.cs
@@ -11,12 +11,19 @@ namespace aula03
         {
             byte n1=10;
             int num=10;
+            int idade = 1984 + 2022;
             char letra='D';
             float valor=5.3f;
             string nome="Diego";
-
+            float res = n1 + num;
+            float res_idade = idade;
             var nome2="Miguel da Silva";
+
             Console.WriteLine (nome + nome2);
+
+            Console.WriteLine (res);
+
+            Console.WriteLine (res_idade);
           
            
 
---
+++ Aula 04/aula04.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Aula04
+{
+
+    int num=10;
+
+
+    static void Main()
+    {
+        int num=0;
+ 
+        Console.WriteLine (num);
+    }
+
+    void teste(Console.WriteLine (num);)
+    {   int num=10;
+
+        
+
+    }
+
+        
+}
+
+
