ddee806887d1ec79768a2eeb9e3b920e572e3bf2
miguelprofissional <miguelprofissional8@gmail.com>
2022-05-02 14:42:04 -0300

    Aula 07

---
+++ Aula 06/aula06.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula06{
+
+    static void Main(){
+
+        int n1,n2,n3;
+        n1=10; n2=20; n3=30;
+        int res = n1 + n2 + n3;
+
+
+        Console.Write(res);
+    }
+}
